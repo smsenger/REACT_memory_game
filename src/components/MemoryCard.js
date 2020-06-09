ad925f3e86840d71e8f9244fa01a466f931611ef
Saesha Senger <saesha.senger@gmail.com>
2020-06-09 12:20:13 -0400

    adds changes through step 2

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -6,11 +6,30 @@ import React, { Component } from 'react';
 
 
 class MemoryCard extends Component {
+    constructor() {
+        super()
+        this.state={ isFlipped:false }
+    }
+
+    clickHandler() {
+        this.setState({ isFlipped: !this.state.isFlipped});
+    }
+
     render() {
-        // const title = 'Card Title';
+        let innerClass = "MemoryCard_inner"
+        if(this.state.isFlipped) {
+            innerClass += " flipped";
+        }
         return (
-            <div className="MemoryCard">
-                <img src="../cartoon-pow.png" alt="pow! star"></img>
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={innerClass}>
+                    <div className="MemoryCard_back">
+                        <img src="../cartoon-pow.png" alt="pow! star"></img>
+                    </div>
+                    <div className="MemoryCard_front">
+                    ∆
+                    </div>
+                </div>
             </div>
         );
     };
@@ -18,4 +37,3 @@ class MemoryCard extends Component {
 
 
 export default MemoryCard;
-  
